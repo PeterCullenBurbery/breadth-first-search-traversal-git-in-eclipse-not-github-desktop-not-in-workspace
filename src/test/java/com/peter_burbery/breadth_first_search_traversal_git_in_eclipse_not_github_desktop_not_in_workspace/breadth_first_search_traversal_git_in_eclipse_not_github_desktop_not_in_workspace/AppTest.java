481a18a1e892d2f173a44382cd8d5250f0efc5e3
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2024-09-25 17:04:24 -0400

    I am committing.

---
+++ src/test/java/com/peter_burbery/breadth_first_search_traversal_git_in_eclipse_not_github_desktop_not_in_workspace/breadth_first_search_traversal_git_in_eclipse_not_github_desktop_not_in_workspace/AppTest.java
@@ -0,0 +1,19 @@
+package com.peter_burbery.breadth_first_search_traversal_git_in_eclipse_not_github_desktop_not_in_workspace.breadth_first_search_traversal_git_in_eclipse_not_github_desktop_not_in_workspace;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
